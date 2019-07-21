c10ca66e027721c09e58c820e817198842087f44
Andre Williams <mMmCheesecake@Andres-MBP.lan1>
2019-07-21 03:39:11 -0400

    Logical Operators

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,19 @@
 console.log("Up and Running!")
 
-var cardOne = 'Queen';
-var cardTwo = 'King';
-var cardThree = 'Queen';
-var cardFour = 'King';
+var cards = ["Queen", "Queen", "King", "King"]
 
-console.log("User flipped" + cardOne)
-console.log("User flipped" + cardTwo)
-console.log("User flipped" + cardThree)
-console.log("User flipped" + cardFour)
+var cardsInPlay = []
+
+var cardOne = cards[0]
+var cardTwo = cards[1]
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry try again");
+	}
+}
