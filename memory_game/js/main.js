d456ea67138cb1d713d0d8ca7ded9802e7666b0c
Andre Williams <mMmCheesecake@Andres-MBP.lan1>
2019-07-21 00:50:57 -0400

    Created Game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,11 @@
+console.log("Up and Running!")
+
+var cardOne = 'Queen';
+var cardTwo = 'King';
+var cardThree = 'Queen';
+var cardFour = 'King';
+
+console.log("User flipped" + cardOne)
+console.log("User flipped" + cardTwo)
+console.log("User flipped" + cardThree)
+console.log("User flipped" + cardFour)
