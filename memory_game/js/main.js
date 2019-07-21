e0c20f71f2b3a6ef87ce337c8ba5383ac284bdd3
Andre Williams <mMmCheesecake@Andres-MBP.lan1>
2019-07-21 18:20:07 -0400

    Added Objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,46 @@
 console.log("Up and Running!")
 
-var cards = ["Queen", "Queen", "King", "King"]
+var cards = [
+{
+rank: "Queen",
+suit: "Diamond",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "King",
+suit: "Diamond",
+cardImage: "images/king-of-diamonds.png"
+},
+{
+rank: "Queen",
+suit: "Heart",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "King",
+suit: "Heart",
+cardImage: "images/king-of-hearts.png"
+}
+];
 
 var cardsInPlay = []
 
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-		console.log("Sorry, try again.")
+		alert("Sorry, try again.")
 	}
 }
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId])
-	cardsInPlay.push(cards[cardId])
-	checkForMatch()
+	cardsInPlay.push(cards[cardId].rank)
+	console.log("User flipped " + cards[cardId].rank)
+	console.log(cards[cardId].cardImage)
+	console.log(cards[cardId].suit)
 }
 
 flipCard(0);
-flipCard(1);
+flipCard(0);
+checkForMatch();
+
